17354ebcd773f26526d313eb079410482f200014
neojohny <yzh2013@yahoo.com>
2014-08-24 12:28:39 -0500

    file changes
    changed the file cachematrx.R so that I can submit my assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two funcitons will set the matrix and calculate 
+## the inverse of it.
 
-## Write a short comment describing this function
+## This makeMatrix will set up the matrix and it's inverse.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeMatrix <- function(x = numeric()) {
+  ## set the value of the matrix
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  ## get the value of the matrix
+  get <- function() x
+  ## calculate the inverse of the matrix
+  setinv <- function(invmatrix) inv <<- invmatrix
+  getinv <- function() inv
+  
+  ## get the inverse of the matrix
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+} 
 
+## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x=numeric(), ...) {
+  
+  ## get the inverse of the matrix
+  inv<-x$getinv()
+  if(!is.null(inv)){
+    message("getting cached data")
+    return(inv)
+  }
+  ## if not: get the inverse of the matrix   
+  matrix<-x$get()
+  inv<-solve(matrix, ...)
+  ## set the inverse of the matrix 
+  x$setinv(inv)
+  inv
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
